56a6b2d4546607ce54b5e98ef13022f53c217b2a
Palani Johnson <palani.johnson@usu.edu>
2021-03-09 11:05:34 -0700

    modified models, added view

--- genie/apis/views.py
+++ genie/apis/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Event, ParkingLot, ParkingSpot
+
+
+def get_events(request):
+    try:
+        number = int(request.GET['num'])
+        events = Event.objects.order_by('-date')[:number]
+
+        response = JsonResponse(events)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+    except:
+        response = JsonResponse({"error": "Invalid unit conversion request"})
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
+
+def get_lots(request):
+    pass
